e4852ee24795400f12bba86efc6092ce18e9ff86
Zeus <137736223+Lee-jewoo@users.noreply.github.com>
2023-07-21 17:23:00 +0900

    다운캐스팅 적용

--- workshop/04_Java4/shape/ShapeTest.java
+++ workshop/04_Java4/shape/ShapeTest.java
@@ -1,6 +1,6 @@
 package shape;
 
-public class ShapeTest {
+public class ShapeTest2 {
 
 	public static void main(String[] args) {
 		
@@ -12,17 +12,25 @@ public class ShapeTest {
 						 new Rectangle(5, 7, "White")};
 		
 		System.out.println("기본정보");
-		for (Shape s : shape) {
-			System.out.printf("%.1f \t  %s\n", s.getArea(), s.getColors());
+		for (int i=0; i<shape.length; i++) {
+			if (shape[i] instanceof Triangle) {
+				System.out.printf("%s \t %.1f \t  %s\n", "Triangle", shape[i].getArea(), shape[i].getColors());
+			} else if (shape[i] instanceof Rectangle){
+				System.out.printf("%s \t %.1f \t  %s\n", "Rectangle", shape[i].getArea(), shape[i].getColors());
+			}
 		}
-		
-		System.out.println("사이즈를 변경 후 정보");
-		for (Shape s : shape) {
-			s.setResize(5);
-			System.out.printf("%.1f \t  %s\n", s.getArea(), s.getColors());
+		System.out.println();
+		System.out.println("사이즈 변경 후 정보");
+		for (int i=0; i<shape.length; i++) {
+			if (shape[i] instanceof Triangle) {
+				((Triangle)shape[i]).setResize(5);
+				System.out.printf("%s \t %.1f \t  %s\n", "Triangle", shape[i].getArea(), shape[i].getColors());
+			} else if (shape[i] instanceof Rectangle){
+				((Rectangle)shape[i]).setResize(5);
+				System.out.printf("%s \t %.1f \t  %s\n", "Rectangle", shape[i].getArea(), shape[i].getColors());
+			}
 		}
-		
-
+				
 	}
 
 }
