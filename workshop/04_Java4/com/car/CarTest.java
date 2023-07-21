df3bdb72d03a6c404daec67b87075210ec4ad2e1
Zeus <137736223+Lee-jewoo@users.noreply.github.com>
2023-07-21 17:24:09 +0900

    다형성, 다운캐스팅 적용

--- workshop/04_Java4/com/car/CarTest.java
+++ workshop/04_Java4/com/car/CarTest.java
@@ -4,36 +4,33 @@ public class CarTest {
 
 	public static void main(String[] args) {
 		
-		L3 car1 [] = new L3[1];
-		L5 car2 [] = new L5[1];
-		car1[0] = new L3("L3", "1500", 50, 25, 0);
-		car2[0] = new L5("L5", "2000", 70, 35, 0);
+		Car car [] = {new L3("L3", "1500", 50, 25, 0),
+					  new L5("L5", "2000", 70, 35, 0)};
+
 		
 		System.out.println("vehicleName \t engineSize \t oilTank \t oilSize \t distance \t temperature");
 		System.out.println("---------------------------------------------------------------------------------------------");
-		System.out.println(car1[0].getName()+" \t\t "+car1[0].getEngine()+" \t\t "+car1[0].getOilTank()+" \t\t "+car1[0].getOilSize()+" \t\t "+car1[0].getDistance()+" \t\t "+car1[0].getTempGage());
-		System.out.println(car2[0].getName()+" \t\t "+car2[0].getEngine()+" \t\t "+car2[0].getOilTank()+" \t\t "+car2[0].getOilSize()+" \t\t "+car2[0].getDistance()+" \t\t "+car2[0].getTempGage());
+		System.out.println(car[0].getName()+" \t\t "+car[0].getEngine()+" \t\t "+car[0].getOilTank()+" \t\t "+car[0].getOilSize()+" \t\t "+car[0].getDistance()+" \t\t "+((L3)car[0]).getTempGage());
+		System.out.println(car[1].getName()+" \t\t "+car[1].getEngine()+" \t\t "+car[1].getOilTank()+" \t\t "+car[1].getOilSize()+" \t\t "+car[1].getDistance()+" \t\t "+((L5)car[1]).getTempGage());
 		System.out.println();
 		
-		car1[0].setOil(25);
-		car2[0].setOil(25);
+		car[0].setOil(25);
+		car[1].setOil(25);
 		System.out.println("25 주유");
 		System.out.println("vehicleName \t engineSize \t oilTank \t oilSize \t distance \t temperature");
 		System.out.println("---------------------------------------------------------------------------------------------");
-		System.out.println(car1[0].getName()+" \t\t "+car1[0].getEngine()+" \t\t "+car1[0].getOilTank()+" \t\t "+car1[0].getOilSize()+" \t\t "+car1[0].getDistance()+" \t\t "+car1[0].getTempGage());
-		System.out.println(car2[0].getName()+" \t\t "+car2[0].getEngine()+" \t\t "+car2[0].getOilTank()+" \t\t "+car2[0].getOilSize()+" \t\t "+car2[0].getDistance()+" \t\t "+car2[0].getTempGage());
+		System.out.println(car[0].getName()+" \t\t "+car[0].getEngine()+" \t\t "+car[0].getOilTank()+" \t\t "+car[0].getOilSize()+" \t\t "+car[0].getDistance()+" \t\t "+((L3)car[0]).getTempGage());
+		System.out.println(car[1].getName()+" \t\t "+car[1].getEngine()+" \t\t "+car[1].getOilTank()+" \t\t "+car[1].getOilSize()+" \t\t "+car[1].getDistance()+" \t\t "+((L5)car[1]).getTempGage());
 		System.out.println();
 		
-		car1[0].go(80);
-		car2[0].go(80);
+		car[0].go(80);
+		car[1].go(80);
 		System.out.println("80 주행");
 		System.out.println("vehicleName \t engineSize \t oilTank \t oilSize \t distance \t temperature");
 		System.out.println("---------------------------------------------------------------------------------------------");
-		System.out.println(car1[0].getName()+" \t\t "+car1[0].getEngine()+" \t\t "+car1[0].getOilTank()+" \t\t "+car1[0].getOilSize()+" \t\t "+car1[0].getDistance()+" \t\t "+car1[0].getTempGage());
-		System.out.println(car2[0].getName()+" \t\t "+car2[0].getEngine()+" \t\t "+car2[0].getOilTank()+" \t\t "+car2[0].getOilSize()+" \t\t "+car2[0].getDistance()+" \t\t "+car2[0].getTempGage());
-
-		
-		
+		System.out.println(car[0].getName()+" \t\t "+car[0].getEngine()+" \t\t "+car[0].getOilTank()+" \t\t "+car[0].getOilSize()+" \t\t "+car[0].getDistance()+" \t\t "+((L3)car[0]).getTempGage());
+		System.out.println(car[1].getName()+" \t\t "+car[1].getEngine()+" \t\t "+car[1].getOilTank()+" \t\t "+car[1].getOilSize()+" \t\t "+car[1].getDistance()+" \t\t "+((L5)car[1]).getTempGage());
+		System.out.println();
 		
 	}
 
