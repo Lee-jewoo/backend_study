81cf2ed9506067c9c98b8f000feb328a1bfbdbf3
Zeus <137736223+Lee-jewoo@users.noreply.github.com>
2023-08-02 18:48:26 +0900

    insert update 추가

--- 07_JDBC/DeptMain.java
+++ 07_JDBC/DeptMain.java
@@ -1,6 +1,7 @@
 import java.util.List;
 import java.util.Scanner;
 import com.dto.DeptDTO;
+import com.exception.DuplicatedDeptnoException;
 import com.service.DeptService;
 import com.service.DeptServiceImpl;
 
@@ -12,6 +13,8 @@ public class DeptMain {
 		while (true) {
 			Scanner scan = new Scanner(System.in);
 			System.out.println("1. 전체 목록");
+			System.out.println("2. 데이터 저장");
+			System.out.println("3. 데이터 수정");
 			System.out.println("0. 종료");
 			System.out.println("----------------");
 			String num = scan.nextLine();
@@ -24,6 +27,37 @@ public class DeptMain {
 				for (DeptDTO dto : list) {
 					System.out.println(dto);
 				}
+			} else if("2".equals(num)) {
+				System.out.println("부서번호를 입력하세요.");
+				String deptno = scan.next();
+				System.out.println("부서명을 입력하세요.");
+				String dname = scan.next();
+				System.out.println("부서위치를 입력하세요.");
+				String loc = scan.next();
+				// 입력받은 값을 DTO에 저장 -> service -> DAO
+				DeptDTO dto = new DeptDTO(Integer.parseInt(deptno), dname, loc);
+				// service와 연동
+				DeptService service = new DeptServiceImpl();
+				int n=0;
+				try {
+					n = service.insert(dto);
+				} catch (DuplicatedDeptnoException e) {
+					System.out.println(e.getMessage());
+				}
+				System.out.println(n+"개가 저장되었습니다.");
+			} else if("3".equals(num)) {
+				System.out.println("수정할 부서번호를 입력하세요.");
+				String deptno = scan.next();
+				System.out.println("수정할 부서명을 입력하세요.");
+				String dname = scan.next();
+				System.out.println("수정할 부서위치를 입력하세요.");
+				String loc = scan.next();
+				// 입력받은 값을 DTO에 저장 -> service -> DAO
+				DeptDTO dto = new DeptDTO(Integer.parseInt(deptno), dname, loc);
+				// service와 연동
+				DeptService service = new DeptServiceImpl();
+				int n = service.update(dto);
+				System.out.println(n+"개가 수정되었습니다.");
 			} else if("0".equals(num)) {
 				System.out.println("프로그램 종료");
 				System.exit(0);
