994c379a8ad5764cb4c06a2f78b340df80018d08
Zeus <137736223+Lee-jewoo@users.noreply.github.com>
2023-08-04 17:20:07 +0900

    delete 추가

--- 07_JDBC/com/service/DeptServiceImpl.java
+++ 07_JDBC/com/service/DeptServiceImpl.java
@@ -96,5 +96,62 @@ public class DeptServiceImpl implements DeptService {
 		return n;
 	}
 	
+	// delete 기능을 수행하는 method
+	// Connection 까지 얻기 -> 이후 작업은 DAO에서 수행
+	@Override
+	public int delete(int deptno){
+		int n = 0;
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(url, userid, passwd);
+			// DAO 연동
+			DeptDAO dao = new DeptDAO();
+			n = dao.delete(con, deptno);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (con != null)
+					con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return n;
+	}
 
+	@Override
+	public int update_delete(DeptDTO dto, int deptno) {
+		int n = 0;
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(url, userid, passwd);
+			con.setAutoCommit(false);
+			DeptDAO dao = new DeptDAO();
+			// 트랜잭션 : 여러 작업을 동시에 처리, 커밋
+			// 하나라도 실패하면 롤백 -> 오토커밋 비활성화 필요
+			n = dao.update(con, dto);
+			n = dao.delete(con, deptno);
+			con.commit();
+		} catch (SQLException e) {
+			try {
+				if (con != null) {
+					con.rollback();
+				}
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		} finally {
+			try {
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return n;
+	}
+	
 }
