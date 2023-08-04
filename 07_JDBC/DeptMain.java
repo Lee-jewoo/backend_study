8f4f0cb65affd08f34410ca8f6b90617ddd7c310
Zeus <137736223+Lee-jewoo@users.noreply.github.com>
2023-08-04 17:09:28 +0900

    30일차 강의 내용 코드 230804 mybatis select

--- 07_JDBC/DeptMain.java
+++ 07_JDBC/DeptMain.java
@@ -1,69 +1,74 @@
+import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Scanner;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 import com.dto.DeptDTO;
-import com.exception.DuplicatedDeptnoException;
-import com.service.DeptService;
-import com.service.DeptServiceImpl;
 
 public class DeptMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
-		// 화면 처리 추가
-		while (true) {
-			Scanner scan = new Scanner(System.in);
-			System.out.println("1. 전체 목록");
-			System.out.println("2. 데이터 저장");
-			System.out.println("3. 데이터 수정");
-			System.out.println("0. 종료");
-			System.out.println("----------------");
-			String num = scan.nextLine();
-			if("1".equals(num)) {
-				// DeptService 연동
-				DeptService service = new DeptServiceImpl();
-				List<DeptDTO> list = service.findAll();
-				System.out.println("부서번호\t부서명\t부서위치");
-				System.out.println("-----------------------------");
-				for (DeptDTO dto : list) {
-					System.out.println(dto);
-				}
-			} else if("2".equals(num)) {
-				System.out.println("부서번호를 입력하세요.");
-				String deptno = scan.next();
-				System.out.println("부서명을 입력하세요.");
-				String dname = scan.next();
-				System.out.println("부서위치를 입력하세요.");
-				String loc = scan.next();
-				// 입력받은 값을 DTO에 저장 -> service -> DAO
-				DeptDTO dto = new DeptDTO(Integer.parseInt(deptno), dname, loc);
-				// service와 연동
-				DeptService service = new DeptServiceImpl();
-				int n=0;
-				try {
-					n = service.insert(dto);
-				} catch (DuplicatedDeptnoException e) {
-					System.out.println(e.getMessage());
-				}
-				System.out.println(n+"개가 저장되었습니다.");
-			} else if("3".equals(num)) {
-				System.out.println("수정할 부서번호를 입력하세요.");
-				String deptno = scan.next();
-				System.out.println("수정할 부서명을 입력하세요.");
-				String dname = scan.next();
-				System.out.println("수정할 부서위치를 입력하세요.");
-				String loc = scan.next();
-				// 입력받은 값을 DTO에 저장 -> service -> DAO
-				DeptDTO dto = new DeptDTO(Integer.parseInt(deptno), dname, loc);
-				// service와 연동
-				DeptService service = new DeptServiceImpl();
-				int n = service.update(dto);
-				System.out.println(n+"개가 수정되었습니다.");
-			} else if("0".equals(num)) {
-				System.out.println("프로그램 종료");
-				System.exit(0);
-			}
-		}
+		// 설정 정보 불러오기 -> import, 예외 처리
+		String resource = "com/config/Configuration.xml"; // Configuration.xml의 위치
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactory sqlSessionFactory =
+		  new SqlSessionFactoryBuilder().build(inputStream);
+		// session 얻기
+		SqlSession session = sqlSessionFactory.openSession();
 
+		// method 호출
+		// 단일행 호출
+		// findByDeptno
+		DeptDTO dto = session.selectOne("DeptMapper.findByDeptno", 10);
+		System.out.println("findByDeptno---------------------");
+		System.out.println(dto);
+		
+		// 복수행 호출
+		// findAll
+		List<DeptDTO> list = session.selectList("DeptMapper.findAll");
+		System.out.println("findAll--------------------------");
+		for (DeptDTO d : list) {
+			System.out.println(d);
+		}
+		
+		// findByDeptnoAndDname
+		DeptDTO dto2 = new DeptDTO();
+		dto2.setDeptno(10);
+		dto2.setDname("개");
+		List<DeptDTO> list2 = session.selectList("DeptMapper.findByDeptnoAndDname", dto2);
+		System.out.println("findByDeptnoAndDname-------------");
+		for (DeptDTO t : list2) {
+			System.out.println(t);
+		}
+		
+		// findByDeptnoAndDnameMap
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("xx", 10);
+		map.put("yy", "개발");
+		List<DeptDTO> list3 = session.selectList("DeptMapper.findByDeptnoAndDnameMap", map);
+		System.out.println("findByDeptnoAndDnameMap----------");
+		for (DeptDTO dto3 : list3) {
+			System.out.println(dto3);
+		}
+		
+		// findAllPage
+		RowBounds bounds = new RowBounds (0, 3); // 1번 행부터 3개
+		List<DeptDTO> list4 = session.selectList("DeptMapper.findAllPage", null, bounds);
+		System.out.println("findAllPage----------------------");
+		for (DeptDTO dto4 : list4) {
+			System.out.println(dto4);
+		}
+		
+		
+		// session 닫기
+		session.close();
+		
 	}
 
 }
