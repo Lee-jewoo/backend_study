200d5937ac6f4f3fd8c3ee9f4dd8a03365bd07d5
Zeus <137736223+Lee-jewoo@users.noreply.github.com>
2023-08-24 19:49:32 +0900

    42일차 강의 내용 코드 테이블 생성

---
+++ 08_JSP_Servlet/10_ShopMiniMall/member.sql
@@ -0,0 +1,14 @@
+-- 회원
+create table member
+   ( userid varchar2(20) CONSTRAINT member_userid_pk PRIMARY KEY,
+     passwd varchar2(20) not null,
+     username varchar2(20) not null,
+     post varchar2(6) not null,
+     addr1 varchar2(500) not null,
+     addr2 varchar2(500) not null,
+     phone1 varchar2(3) not null,
+     phone2 varchar2(4) not null,
+     phone3 varchar2(4) not null,
+     email1 varchar2(20) not null,
+     email2 varchar2(20) not null
+    );
