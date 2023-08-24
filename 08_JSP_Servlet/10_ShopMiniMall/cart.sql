cb4b0d869c8c689b42df8b3528265097a55ea18e
Zeus <137736223+Lee-jewoo@users.noreply.github.com>
2023-08-24 19:50:34 +0900

    42일차 강의 내용 코드 테이블 생성

---
+++ 08_JSP_Servlet/10_ShopMiniMall/cart.sql
@@ -0,0 +1,21 @@
+-- cart 
+  create table cart
+  (  num NUMBER(6) PRIMARY KEY,
+     userid VARCHAR2(20),
+     gCode varchar2(20) not null,
+     gName varchar2(50) not null,
+     gPrice NUMBER(6) not null,
+     gSize CHAR(1) not null,
+     gColor VARCHAR2(10) not null,
+     gAmount NUMBER(2) not null,
+     gImage varchar2(20) not null
+  );   
+  alter table cart
+  add CONSTRAINT cart_userid_fk FOREIGN KEY(userid)
+   REFERENCES member(userid) ON DELETE CASCADE;
+   
+  alter table cart
+  add CONSTRAINT cart_gCode_fk FOREIGN KEY(gCode)
+   REFERENCES goods(gCode) ON DELETE CASCADE; 
+   
+ create sequence cart_seq;
