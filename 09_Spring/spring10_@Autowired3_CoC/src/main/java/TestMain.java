1b594d086a1b25355167d1dad86d05bb7c55736c
Zeus <137736223+Lee-jewoo@users.noreply.github.com>
2023-08-30 17:32:08 +0900

    46일차 강의 내용 코드 230830 @Autowired

---
+++ 09_Spring/spring10_@Autowired3_CoC/src/main/java/TestMain.java
@@ -0,0 +1,17 @@
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import com.dto.Person;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+
+		ApplicationContext ctx = new GenericXmlApplicationContext("classpath:com/config/user.xml");
+		Person person = ctx.getBean("person", Person.class);
+		System.out.println(person);
+		
+	}
+
+}
