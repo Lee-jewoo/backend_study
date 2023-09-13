84c7603bc5231093c770f44e5bd2f4e144e232a0
Zeus <137736223+Lee-jewoo@users.noreply.github.com>
2023-09-13 17:39:50 +0900

    53일차 강의 내용 코드 230913 spring MVC

---
+++ 09_Spring/spring17_MVC/src/main/java/com/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping("/xxx")
+	public String home() {
+		System.out.println("TestController.home");
+		return "/WEB-INF/views/hello.jsp";
+	}
+	
+	@RequestMapping(value="/yyy")
+	public String main() {
+		System.out.println("TestController.main");
+		return "/WEB-INF/views/hello.jsp";
+	}
+
+}
